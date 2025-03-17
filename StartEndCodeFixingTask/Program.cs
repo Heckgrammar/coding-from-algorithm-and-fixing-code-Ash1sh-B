0ea5b1b5b2dcfd23ddfd287722e17e31973aadcc
Ash1sh-B <156790016+Ash1sh-B@users.noreply.github.com>
2025-03-17 21:13:49 +0000

    Update Program.cs

--- StartEndCodeFixingTask/Program.cs
+++ StartEndCodeFixingTask/Program.cs
@@ -4,10 +4,13 @@
     {
         static void Main(string[] args)
         {
-            bool ValidChoice = false
+            bool ValidChoice = false;
             int difference = -1
-            Console.Writeline("enter the start year")
-            int Start = Convert.ToInt32(Console.Readline())
+            Console.Writeline("enter the start year");
+            int Start = Convert.ToInt32(Console.Readline());
+            Console.Writeline("enter the end year");
+            int End = Convert.Toint32(console.readline());
+            if (start
                 
             
             // The algorithm in the Readme contains an error
